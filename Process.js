ebd6c85d97ad0df1635c00fadf2040a017c37093
qinzijun <qinzijun@dias.com.cn>
2021-01-27 16:06:42 +0800

    v1.3：修复输出长度不正确的BUG；修复尾部CRC时的填充BUG

--- Process.js
+++ Process.js
@@ -133,7 +133,7 @@ WorkerScript.onMessage = function(msg) {
 
         //内容裁剪
         if(output[i].headOrTail === 'tail') {
-            output[i].content = output[i].content.slice(0, output[i].maxAddr - output[i].startAddr + 5);
+            output[i].content = output[i].content.slice(0, output[i].maxAddr - output[i].startAddr + 3);
         }else if(output[i].headOrTail === 'head') {
             var rem = (output[i].maxAddr + 1) % 0x100;
             if(rem !== 0) {
@@ -179,7 +179,7 @@ WorkerScript.onMessage = function(msg) {
         for(var j = 0; j < output[i].content.length; j++) {
             var data = output[i].content[j];
             var dataAddr = padding((output[i].startAddr + j).toString(16), 8).toUpperCase();
-            var remainLen = output[i].endAddr - output[i].startAddr + 1 - j;
+            var remainLen = output[i].maxAddr - output[i].startAddr + 1 - j;
 
             if(insertELA) {  // 插入扩展线性地址记录
                 outText += ':02000004' + dataAddr.slice(0, 4);
